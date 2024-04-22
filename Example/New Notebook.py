7462be2e675af6c49d483fd4503b9d8ad7bd4f2e
mpsmyth71@hotmail.com <mpsmyth71@hotmail.com>
2024-04-22 15:10:14 +0000

    Add New Notebook

---
+++ Example/New Notebook.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Generate the markdown header
+# MAGIC markdown_header = "# My Markdown Header"
+# MAGIC
+# MAGIC # Generate the markdown code block
+# MAGIC markdown_code_block = "```python\n# My Python Code\nprint('Hello, World!')\n```"
